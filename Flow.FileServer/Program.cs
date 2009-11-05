d5e8bfe2f52b7c6fa04ab32324650e8e757f0b45
Dykam <kramieb@gmail.com>
2009-11-05 13:16:53 +0100

    changes

--- Flow.FileServer/Program.cs
+++ Flow.FileServer/Program.cs
@@ -10,16 +10,21 @@ namespace Flow.FileServer
 	{
 		static void Main(string[] args)
 		{
-			var router = new Router();
+			var router = new Router(8080);
 			router
-				.If(request => true)
+				.If(request => request.Path.Contains("ah"))
 				.RespondWith(request =>
 				{
 					var address = (IPEndPoint)request.Client.Client.RemoteEndPoint;
+					Console.WriteLine(address);
 					request
 						.Respond(200)
-						.StreamText(String.Format("It works! Oh, and I know your IP, I think it is this one: {0}", address));
+						.StreamText(String.Format(
+@"It works! Oh, and I know your IP, I think it is this one: {0}
+And you requested ""{1}"""
+								                          , address, request.Path));
 				});
 			router.Start();
 		}
-	}}
+	}
+}
